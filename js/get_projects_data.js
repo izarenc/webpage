80b8243c93bc18449e8b3650a3dd163f7ba2d55b
Iza <izarenc@onet.eu>
2021-06-21 14:29:23 +0200

    about me; modify list of imgs in js file via Python script

--- js/get_projects_data.js
+++ js/get_projects_data.js
@@ -1,7 +1,8 @@
 
  $(document).ready(function(){  
-   
-    let img_files = ['projects_data\\blood_vessels\\crossP.PNG', 'projects_data\\blood_vessels\\crossV.PNG', 'projects_data\\blood_vessels\\handP.PNG', 'projects_data\\blood_vessels\\handV.PNG', 'projects_data\\blood_vessels\\squareP.PNG', 'projects_data\\blood_vessels\\squareV.PNG', 'projects_data\\blood_vessels\\deep\\zigzagP.PNG', 'projects_data\\blood_vessels\\deep\\zigzagV.PNG', 'projects_data\\heart_rate_simulator\\circuit.jpg']
+    //python generated data begin
+		let img_files = ['projects_data\\blood_vessels\\crossP.PNG', 'projects_data\\blood_vessels\\crossV.PNG', 'projects_data\\blood_vessels\\handP.PNG', 'projects_data\\blood_vessels\\handV.PNG', 'projects_data\\blood_vessels\\squareP.PNG', 'projects_data\\blood_vessels\\squareV.PNG', 'projects_data\\blood_vessels\\zigzagP.PNG', 'projects_data\\blood_vessels\\zigzagV.PNG', 'projects_data\\heart_rate_simulator\\circuit.jpg']; 
+ 		//python generated data end
 
     for(var i=0 ; i< img_files.length ; i++) {
         var path="./img/Figure_"+i+".png";
--- path_gethering_script.py
+++ path_gethering_script.py
@@ -1,18 +1,43 @@
 import os
 
-dirname = os.path.dirname(__file__)
-project_folder_path = os.path.join(dirname, "projects_data")
+debug=0
 
-f = []
-for (dirpath, dirnames, filenames) in os.walk(project_folder_path):
-    for name in filenames:
-        full_path = os.path.join(dirpath, name)
-
-        path = os.path.relpath(full_path, dirname)
-        if path[-3:].lower() in ['jpg', 'png', 'bmp']:
-            f.append( path )
-#jpg, png bmp 
-for idx, it in enumerate(f):
-    print(idx,it)
+def get_all_paths(debug=0):
+    dirname = os.path.dirname(__file__)
+    project_folder_path = os.path.join(dirname, "projects_data")
     
-print(f)
+    result_paths = []
+    for (dirpath, dirnames, filenames) in os.walk(project_folder_path):
+        for name in filenames:
+            full_path = os.path.join(dirpath, name)
+    
+            path = os.path.relpath(full_path, dirname)
+            if path[-3:].lower() in ['jpg', 'png', 'bmp']:
+                result_paths.append( path )
+    
+    if debug:
+        for idx, it in enumerate(result_paths):
+            print(idx,it)
+            
+        print(result_paths)
+    return result_paths
+
+images_paths = get_all_paths(debug)
+
+dirname = os.path.dirname(__file__)
+file_path = os.path.join(dirname, 'js\\get_projects_data.js')
+
+with open(file_path, 'r') as file :
+  filedata = file.read()
+
+begin_tag = r'//python generated data begin'
+end_tag = r'//python generated data end'
+begin = filedata.find(begin_tag)
+end = filedata.find(end_tag)
+
+insertion = r"let img_files = " + repr(images_paths) + ";"
+new_content = filedata[:begin] + begin_tag + "\n\t\t" + insertion + " \n \t\t" + filedata[end:]
+#let img_files = [
+    
+with open(file_path, 'w') as file:
+  file.write(new_content)
